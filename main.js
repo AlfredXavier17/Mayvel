ef41b1f2e0cb7c393a62680dd7ffdb870278b243
Alfred Xavier C.T <74560489+AlfredXavier17@users.noreply.github.com>
2025-01-26 13:51:27 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+// Updated JavaScript - main.js
+const container = document.getElementById("container");
+const imageOne = document.querySelector(".image-1");
+const imageTwo = document.querySelector(".image-2");
+const btnYes = document.querySelector(".btn-yes");
+const btnNo = document.querySelector(".btn-no");
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+btnNo.addEventListener("mouseover", (event) => {
+  const containerRect = container.getBoundingClientRect();
+  const btnRect = btnNo.getBoundingClientRect();
+
+  let newTop, newLeft;
+
+  do {
+    newTop = getRandomNumber(0, containerRect.height - btnRect.height);
+  } while (Math.abs(newTop - btnRect.top) < containerRect.height / 3);
+
+  do {
+    newLeft = getRandomNumber(0, containerRect.width - btnRect.width);
+  } while (Math.abs(newLeft - btnRect.left) < containerRect.width / 3);
+
+  btnNo.style.position = "absolute";
+  btnNo.style.top = `${newTop}px`;
+  btnNo.style.left = `${newLeft}px`;
+});
+
+btnYes.addEventListener("click", () => {
+  btnNo.classList.add("hide");
+  imageOne.classList.add("hide");
+  imageTwo.classList.remove("hide");
+});
